08f4b834ac3572caf7f1381aa41c3e571800bd2a
Russell McWhae <russell.mcwhae@gmail.com>
2019-08-12 22:23:30 +0000

    Beautified code to use single quotes and updated vars to use const rather than let; additional refactoring

--- express_server.js
+++ express_server.js
@@ -1,57 +1,103 @@
 /* SERVER INITIALIZATION */
-const express = require("express");
+const express = require('express');
 const app = express();
 const PORT = 8080;
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const cookieSession = require('cookie-session');
 const methodOverride = require('method-override');
 const bcrypt = require('bcrypt');
-const { generateRandomString, getUserByEmail, urlsForUser, validShortUrl, filterVisitsByShortURL } = require('./helpers');
+const {
+  generateRandomString,
+  getUserByEmail,
+  urlsForUser,
+  validShortUrl,
+  filterVisitsByShortURL
+} = require('./helpers');
 const serverStartTime = new Date();
 
-app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 app.use(cookieParser());
-app.use(cookieSession({
-  name: 'session',
-  keys: ['key1acnbcbbc', 'key2acnacnacn'],
-  // Cookie Options
-  maxAge: 24 * 60 * 60 * 1000 // 24 hours
-}));
+app.use(
+  cookieSession({
+    name: 'session',
+    keys: ['key1acnbcbbc', 'key2acnacnacn'],
+    // Cookie Options
+    maxAge: 24 * 60 * 60 * 1000 // 24 hours
+  })
+);
 app.use(methodOverride('_method'));
 
-/* "DATABASES" (notice quotes…) */
+/* 'DATABASES' (notice quotes…) */
 
 const urlDatabase = {
-  "b2xVn2": { longURL: "http://www.lighthouselabs.ca", userID: "x43d4r", totalVisits: 3, uniqueVisits: 0, date: serverStartTime },
-  "9sm5xK": { longURL: "http://www.google.com", userID: "x43d4r", totalVisits: 3, uniqueVisits: 0, date: serverStartTime },
-  "57fh37": { longURL: "http://www.engadget.com", userID: "gt7574", totalVisits: 3, uniqueVisits: 0, date: serverStartTime }
+  b2xVn2: {
+    longURL: 'http://www.lighthouselabs.ca',
+    userID: 'x43d4r',
+    totalVisits: 3,
+    uniqueVisits: 0,
+    date: serverStartTime
+  },
+  '9sm5xK': {
+    longURL: 'http://www.google.com',
+    userID: 'x43d4r',
+    totalVisits: 3,
+    uniqueVisits: 0,
+    date: serverStartTime
+  },
+  '57fh37': {
+    longURL: 'http://www.engadget.com',
+    userID: 'gt7574',
+    totalVisits: 3,
+    uniqueVisits: 0,
+    date: serverStartTime
+  }
 };
 
 const users = {
-  "x43d4r": {
-    id: "x43d4r",
-    email: "russell@a.com",
-    password: "$2b$10$IRUSZoB4dctyflA4lmWhJukbQS6nMZ1FhiBLE6Ka7VCRyrjMbACqS"
+  x43d4r: {
+    id: 'x43d4r',
+    email: 'russell@a.com',
+    password: '$2b$10$IRUSZoB4dctyflA4lmWhJukbQS6nMZ1FhiBLE6Ka7VCRyrjMbACqS'
   },
-  "gt7574": {
-    id: "gt7574",
-    email: "russell@b.com",
-    password: "$2b$10$LemPBJdGHiuYS5gz1LmrLeAx1TZa4mTyqK5R61OmRQvMlobgMq4Oy"
+  gt7574: {
+    id: 'gt7574',
+    email: 'russell@b.com',
+    password: '$2b$10$LemPBJdGHiuYS5gz1LmrLeAx1TZa4mTyqK5R61OmRQvMlobgMq4Oy'
   }
-};// user a pw plaintext: stuff; user b pw plaintext: things
+}; // user a pw plaintext: stuff; user b pw plaintext: things
 
 const allVisits = {
-  'fghfy8': { visitorId: 'fghfy8', visitDate: serverStartTime, shortURLVisited: "b2xVn2" },
-  '9ud75h': { visitorId: '9ud75h', visitDate: serverStartTime, shortURLVisited: "b2xVn2" },
-  'f85ur7': { visitorId: 'f85ur7', visitDate: serverStartTime, shortURLVisited: "b2xVn2" },
-  '85ht7r': { visitorId: '85ht7r', visitDate: serverStartTime, shortURLVisited: "9sm5xK" }
+  fghfy8: {
+    visitorId: 'fghfy8',
+    visitDate: serverStartTime,
+    shortURLVisited: 'b2xVn2'
+  },
+  '9ud75h': {
+    visitorId: '9ud75h',
+    visitDate: serverStartTime,
+    shortURLVisited: 'b2xVn2'
+  },
+  f85ur7: {
+    visitorId: 'f85ur7',
+    visitDate: serverStartTime,
+    shortURLVisited: 'b2xVn2'
+  },
+  '85ht7r': {
+    visitorId: '85ht7r',
+    visitDate: serverStartTime,
+    shortURLVisited: '9sm5xK'
+  }
 };
 
 /* GET REQUEST ROUTING */
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   if (!req.session.user_id) {
     res.redirect('/login');
   } else {
@@ -59,28 +105,29 @@ app.get("/", (req, res) => {
   }
 });
 
-app.get("/urls", (req, res) => {
-  let userID = req.session.user_id;
-  let templateVars = {
+app.get('/urls', (req, res) => {
+  const userID = req.session.user_id;
+  const templateVars = {
     user: users[userID],
-    urls: urlsForUser(userID, urlDatabase),
+    urls: urlsForUser(userID, urlDatabase)
   };
-  res.render("urls_index", templateVars);
+  res.render('urls_index', templateVars);
 });
 
-app.get("/urls/new", (req, res) => {
-  let templateVars = {
-    user: users[req.session.user_id]
-  };
-  if (!templateVars.user) {
+app.get('/urls/new', (req, res) => {
+  const user = users[req.session.user_id];
+  if (!user) {
     res.redirect('/login');
   } else {
-    res.render("urls_new", templateVars);
+    const templateVars = {
+      user: user
+    };
+    res.render('urls_new', templateVars);
   }
 });
 
-app.get("/urls/:shortURL", (req, res) => {
-  let givenShortURL = req.params.shortURL;
+app.get('/urls/:shortURL', (req, res) => {
+  const givenShortURL = req.params.shortURL;
   const userID = req.session.user_id;
   if (!userID) {
     res.status(403).send('Error: You must be logged in to view this page.');
@@ -89,7 +136,7 @@ app.get("/urls/:shortURL", (req, res) => {
   } else if (userID !== urlDatabase[req.params.shortURL].userID) {
     res.status(403).send('Error: You do not have access to this short URL.');
   } else {
-    let templateVars = {
+    const templateVars = {
       shortURL: givenShortURL,
       longURL: urlDatabase[givenShortURL].longURL,
       user: users[userID],
@@ -98,32 +145,37 @@ app.get("/urls/:shortURL", (req, res) => {
       allVisits: filterVisitsByShortURL(givenShortURL, allVisits),
       visits: urlDatabase[givenShortURL].totalVisits
     };
-    res.render("urls_show", templateVars);
+    res.render('urls_show', templateVars);
   }
 });
 
-app.get("/u/:shortURL", (req, res) => {
-  let givenShortURL = req.params.shortURL;
+app.get('/u/:shortURL', (req, res) => {
+  const givenShortURL = req.params.shortURL;
   if (!validShortUrl(givenShortURL, urlDatabase)) {
     res.status(403).send('Error: Invalid Short URL.');
   } else {
     const longURL = urlDatabase[givenShortURL].longURL;
-    let newVisit = {}; // add a newVisit entry
-    let newVisitID = generateRandomString();
-    newVisit.visitorId = newVisitID;
-    newVisit.visitDate = new Date();
-    newVisit.shortURLVisited = givenShortURL;
-    allVisits[newVisitID] = newVisit;
+    let createdVisit = {}; // add a newVisit entry
+    const newVisitID = generateRandomString();
+    createdVisit.visitorId = newVisitID;
+    createdVisit.visitDate = new Date();
+    createdVisit.shortURLVisited = givenShortURL;
+    allVisits[newVisitID] = createdVisit;
     if (!req.cookies[givenShortURL]) {
       res.cookie(givenShortURL, new Date());
       urlDatabase[givenShortURL].uniqueVisits++;
-    } else { // let's see how old this cookie is
-      let now = new Date();
-      let lastVisitTimeFromCookie = req.cookies[givenShortURL]; // bad idea to store info in cookies and not server side, but this is just an exercise
+    } else {
+      // let's see how old this cookie is
+      const now = new Date();
+      const lastVisitTimeFromCookie = req.cookies[givenShortURL]; // bad idea to store info in cookies and not server side, but this is just an exercise
       // below code adapted from https://stackoverflow.com/questions/7709803/javascript-get-minutes-between-two-dates
-      let dateDifference = Date.parse(now) - Date.parse(lastVisitTimeFromCookie);
-      let dateDifferenceInMinutes = Math.round(((dateDifference % 86400000) % 3600000) / 60000);
-      if (dateDifferenceInMinutes > 30) { // for an existing cookie, give 30 min timeout before considering a new unique visit; per https://matomo.org/faq/general/faq_21418/
+      const dateDifference =
+        Date.parse(now) - Date.parse(lastVisitTimeFromCookie);
+      const dateDifferenceInMinutes = Math.round(
+        ((dateDifference % 86400000) % 3600000) / 60000
+      );
+      if (dateDifferenceInMinutes > 30) {
+        // for an existing cookie, give 30 min timeout before considering a new unique visit; per https://matomo.org/faq/general/faq_21418/
         res.cookie(givenShortURL, new Date());
         urlDatabase[givenShortURL].uniqueVisits++;
       } else {
@@ -140,23 +192,23 @@ app.get('/register', (req, res) => {
   if (userID) {
     res.redirect('/urls');
   } else {
-    let templateVars = {
-      user: users[req.session.user_id]
+    const templateVars = {
+      user: userID
     };
-    res.render("urls_register", templateVars);
+    res.render('urls_register', templateVars);
   }
 });
 
 app.get('/login', (req, res) => {
-  let templateVars = {
+  const templateVars = {
     user: users[req.session.user_id]
   };
-  res.render("urls_login", templateVars);
+  res.render('urls_login', templateVars);
 });
 
 /* POST REQUEST ROUTING */
 
-app.delete("/urls/:shortURL/delete", (req, res) => {
+app.delete('/urls/:shortURL/delete', (req, res) => {
   const userID = req.session.user_id;
   if (!userID) {
     res.status(403).send('Error: You must be logged in to view this page.');
@@ -168,14 +220,14 @@ app.delete("/urls/:shortURL/delete", (req, res) => {
   }
 });
 
-app.post("/urls", (req, res) => {
+app.post('/urls', (req, res) => {
   const userID = req.session.user_id;
   if (!userID) {
     res.status(403).send('Error: You must be logged in to view this page.');
   } else {
-    let newshortID = generateRandomString();
+    const newshortID = generateRandomString();
     let newURLObj = {};
-    newURLObj.longURL = req.body["longURL"];
+    newURLObj.longURL = req.body['longURL'];
     newURLObj.userID = userID;
     newURLObj.totalVisits = 0;
     newURLObj.uniqueVisits = 0;
@@ -185,28 +237,32 @@ app.post("/urls", (req, res) => {
   }
 });
 
-app.put("/urls/:id", (req, res) => {
+app.put('/urls/:id', (req, res) => {
   const userID = req.session.user_id;
   if (!userID) {
-    res.status(403).send('Error: You must be logged in to perform this action.');
+    res
+      .status(403)
+      .send('Error: You must be logged in to perform this action.');
   } else if (userID !== urlDatabase[req.params.id].userID) {
     res.status(403).send('Error: You do not have access to this short URL.');
   } else {
-    let shortcode = req.params.id;
-    urlDatabase[shortcode].longURL = req.body["longURL"]; // update previous long URL with new long URL
+    const shortcode = req.params.id;
+    urlDatabase[shortcode].longURL = req.body['longURL']; // update previous long URL with new long URL
     res.redirect('/urls/');
   }
 });
 
 app.post('/login', (req, res) => {
-  let inputEmail = req.body["email"];
-  let inputPassword = req.body["password"];
-  let existingUserID = getUserByEmail(inputEmail, users);
+  const inputEmail = req.body['email'];
+  const inputPassword = req.body['password'];
+  const existingUserID = getUserByEmail(inputEmail, users);
   if (!inputEmail || !inputPassword) {
     res.status(403).send('Error: Both fields in the form are required.');
   } else if (!existingUserID) {
     res.status(403).send('Error: Email address could not be found.');
-  } else if (!bcrypt.compareSync(inputPassword, users[existingUserID].password)) {
+  } else if (
+    !bcrypt.compareSync(inputPassword, users[existingUserID].password)
+  ) {
     res.status(403).send('Error: Incorrect password.');
   } else {
     // eslint-disable-next-line camelcase
@@ -221,16 +277,25 @@ app.post('/logout', (req, res) => {
 });
 
 app.post('/register', (req, res) => {
-  if (!req.body["password"] || !req.body["email"]) {
-    res.status(400).send('All fields must be filled out. Please return to the previous page and enter all required form inputs.');
-  } else if (getUserByEmail(req.body["email"], users)) {
-    res.status(400).send('Error: Email address has already been registered. Please return to the previous page and use a different email address');
+  if (!req.body['password'] || !req.body['email']) {
+    res
+      .status(400)
+      .send(
+        'All fields must be filled out. Please return to the previous page and enter all required form inputs.'
+      );
+  } else if (getUserByEmail(req.body['email'], users)) {
+    res
+      .status(400)
+      .send(
+        'Error: Email address has already been registered. Please return to the previous page and use a different email address'
+      );
   } else {
-    let newUserID = generateRandomString();
-    users[newUserID] = {};
-    users[newUserID].id = newUserID;
-    users[newUserID].email = req.body["email"];
-    users[newUserID].password = bcrypt.hashSync(req.body["password"], 10);
+    const newUserID = generateRandomString();
+    users[newUserID] = {
+      id: newUserID,
+      email: req.body['email'],
+      password: bcrypt.hashSync(req.body['password'], 10)
+    };
     // eslint-disable-next-line camelcase
     req.session.user_id = newUserID;
     res.redirect('/urls');
--- helpers.js
+++ helpers.js
@@ -1,23 +1,27 @@
 /* FUNCTION DECLARATIONS */
 
-const generateRandomString = function() {// adapted from https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
-  let ret = "";
-  let length = 6;//could also pass in as argument
-  let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+const generateRandomString = function() {
+  // adapted from https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
+  let randomString = '';
+  const length = 6; //could also pass in as argument
+  const characters =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   for (let i = 0; i < length; i++) {
-    ret += characters.charAt(Math.floor(Math.random() * characters.length));
+    randomString += characters.charAt(
+      Math.floor(Math.random() * characters.length)
+    );
   }
-  return ret;
+  return randomString;
 };
 
 const getUserByEmail = function(emailToCheck, objectToCheckIn) {
-  let ret = undefined;
+  let user;
   for (let userID in objectToCheckIn) {
-    if (emailToCheck === objectToCheckIn[userID]["email"]) {
-      ret = userID;
+    if (emailToCheck === objectToCheckIn[userID]['email']) {
+      user = userID;
     }
   }
-  return ret;
+  return user;
 };
 
 const urlsForUser = function(id, objectToCheckIn) {
